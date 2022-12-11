d878f62e19c3e375bfaf8af13d7ab59cd0bb0411
Mariya <marisha_1793@mail.ru>
2022-12-12 00:57:32 +0700

    решение второй задачи

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("1456 Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine(a);
+}
+else 
+if (b > a & b > c)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine(b);
+}
+else
+if (c > a & c > b)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
